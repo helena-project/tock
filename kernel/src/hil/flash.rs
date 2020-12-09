1319d1a5c95be9296a42a0b0ef5812ba320c7407
Alistair Francis <alistair.francis@wdc.com>
2020-12-08 16:30:50 -0800

    hil: Flash: Re-write the flash HILt
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- kernel/src/hil/flash.rs
+++ kernel/src/hil/flash.rs
@@ -21,7 +21,7 @@
 //! impl<const W: usize, const E: usize> flash::Flash<W, E> for NewChipStruct {
 //!     fn read(
 //!         &self,
-//!         address: usize,
+//!         address: u64,
 //!         length: usize,
 //!         buf: &'static mut [u8],
 //!     ) -> Result<(), (ReturnCode, &'static mut [u8])> {
@@ -30,13 +30,13 @@
 //!
 //!     fn write(
 //!         &self,
-//!         address: usize,
-//!         buf: &'static mut[u8; W],
-//!     ) -> Result<(), (ReturnCode, &'static mut [u8; W])> {
+//!         address: u64,
+//!         buf: &'static mut[u8],
+//!     ) -> Result<(), (ReturnCode, &'static mut [u8])> {
 //!         unimplemented!()
 //!     }
 //!
-//!     fn erase_page(&self, page_number: usize) -> Result<(), ReturnCode> {
+//!     fn erase(&self, address: u64, length: usize) -> Result<(), ReturnCode> {
 //!         unimplemented!()
 //!     }
 //! }
@@ -66,7 +66,7 @@
 //!
 //! impl<'a, F: flash::Flash<W, E>, const W: usize, const E: usize> flash::Client<W, E> for FlashUser<'a, F, W, E> {
 //!     fn read_complete(&self, read_buffer: &'static mut [u8], ret: Result<(), ReturnCode>) {}
-//!     fn write_complete(&self, write_buffer: &'static mut [u8; W], ret: Result<(), ReturnCode>) { }
+//!     fn write_complete(&self, write_buffer: &'static mut [u8], ret: Result<(), ReturnCode>) { }
 //!     fn erase_complete(&self, ret: Result<(), ReturnCode>) {}
 //! }
 //! ```
@@ -125,4 +125,89 @@ pub trait LegacyClient<F: LegacyFlash> {
 
     /// Flash erase complete.
     fn erase_complete(&self, error: LegacyError);
+}
+
+/// A page of writeable persistent flash memory.
+///
+/// `W`: Should be the minimum number of bytes that can be written
+///      in an operation.
+/// `E`: Should be the minimum number of bytes that can be erased
+///      in an operation.
+pub trait Flash<const W: usize, const E: usize> {
+    /// Read data from flash into a buffer.
+    ///
+    /// This function will read data stored in flash at `address` and
+    /// `length` into the buffer `buf`.
+    /// `address` is calculated as an offset from the start of the flash
+    /// region.
+    ///
+    /// On success returns nothing
+    /// On failure returns a `ReturnCode` and the buffer passed in.
+    /// If `ReturnCode::ENOSUPPORT` is returned then `read_page()`
+    // should be used instead.
+    fn read(
+        &self,
+        address: u64,
+        length: usize,
+        buf: &'static mut [u8],
+    ) -> Result<(), (ReturnCode, &'static mut [u8])>;
+
+    /// Write data from a buffer to flash.
+    ///
+    /// This function will write the buffer `buf` to the `address` specified
+    /// in flash.
+    ///
+    /// `address` must be aligned to `W`.
+    /// The length of `buf` must be aligned to `W`.
+    ///
+    /// This function will not erase the page first. The user of this function
+    /// must ensure that a page is erased before writing.
+    /// Writes to flash can only turn a `1` to a `0`. To change a `0` to a `1`
+    /// the region must be erased.
+    ///
+    /// Note that some hardware only allows a limited number of writes before
+    /// an erase. If that is the case the implementation MUST return an error
+    /// `ReturnCode::ENOMEM` when this happens, even if the hardware silently
+    /// ignores the write.
+    ///
+    /// On success returns nothing
+    /// On failure returns a `ReturnCode` and the buffer passed in.
+    fn write(
+        &self,
+        address: u64,
+        buf: &'static mut [u8],
+    ) -> Result<(), (ReturnCode, &'static mut [u8])>;
+
+    /// Erase a page/pages of flash, setting every byte to 0xFF.
+    ///
+    /// This will erase all pages starting from `address` for the `length`.
+    /// `address` and `length must allign with `E`.
+    ///
+    /// On success returns nothing
+    /// On failure returns a `ReturnCode`.
+    fn erase(&self, address: u64, length: usize) -> Result<(), ReturnCode>;
+}
+
+/// Implement `Client` to receive callbacks from `Flash`.
+pub trait Client<const W: usize, const E: usize> {
+    /// Flash read complete.
+    ///
+    /// This will be called when the read operation is complete.
+    /// On success `ret` will be nothing.
+    /// On error `ret` will contain a `ReturnCode`
+    fn read_complete(&self, read_buffer: &'static mut [u8], ret: Result<(), ReturnCode>);
+
+    /// Flash write complete.
+    ///
+    /// This will be called when the write operation is complete.
+    /// On success `ret` will be nothing.
+    /// On error `ret` will contain a `ReturnCode`
+    fn write_complete(&self, write_buffer: &'static mut [u8], ret: Result<(), ReturnCode>);
+
+    /// Flash erase complete.
+    ///
+    /// This will be called when the erase operation is complete.
+    /// On success `ret` will be nothing.
+    /// On error `ret` will contain a `ReturnCode`
+    fn erase_complete(&self, ret: Result<(), ReturnCode>);
 }
